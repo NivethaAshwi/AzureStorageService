e40666b347e04b6346771f659f6d758c25f33d32
NivethaAshwi <133736462+NivethaAshwi@users.noreply.github.com>
2023-06-16 10:25:59 +0530

    Update ItemsController.cs

--- AzureStorageswithDemoApplication/Controllers/ItemsController.cs
+++ AzureStorageswithDemoApplication/Controllers/ItemsController.cs
@@ -1,4 +1,4 @@
-﻿using AzureStorage.Service.Service;
+using AzureStorage.Service.Service;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using AzureStorage.Models.TableStorageModel;
@@ -9,40 +9,79 @@ namespace AzureStorageswithDemoApplication.Controllers
     [ApiController]
     public class ItemsController : ControllerBase
     {
-        private readonly IAzureTableStorage _storageService;
-        public ItemsController(IAzureTableStorage storageService)
+        private readonly IAzureTableStorage _tablestorageService;
+        private readonly ILogger<ItemsController> _logger;
+        public ItemsController(IAzureTableStorage storageService, ILogger<ItemsController> logger)
         {
-            _storageService = storageService ?? throw new ArgumentNullException(nameof(storageService));
+            _tablestorageService = storageService ?? throw new ArgumentNullException(nameof(storageService));
+            _logger = logger;
+
         }
         [HttpGet]
-        [ActionName(nameof(GetAsync))]
-        public async Task<IActionResult> GetAsync([FromQuery] string category, string id)
+        [ActionName(nameof(GetGroceryItems))]
+        public async Task<IActionResult> GetGroceryItems([FromQuery] string category, string id)
         {
-            return Ok(await _storageService.GetEntityAsync(category, id));
+            try
+            {
+                return Ok(await _tablestorageService.GetGroceryDetails(category, id));
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Error try to Get Grocery item Table details :{ex.StackTrace}{ex.InnerException}");
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                    "Error retrieving data from Azure Table");
+            }
         }
         [HttpPost]
-        public async Task<IActionResult> PostAsync([FromBody] GroceryItemEntity entity)
+        public async Task<IActionResult> InsertGroceryEntity([FromBody] GroceryItemEntity entity)
         {
-            entity.PartitionKey = entity.Category;
-            string Id = Guid.NewGuid().ToString();
-            entity.Id = Id;
-            entity.RowKey = Id;
-            var createdEntity = await _storageService.UpsertEntityAsync(entity);
-            return CreatedAtAction(nameof(GetAsync), createdEntity);
+            try
+            {
+                entity.PartitionKey = entity.Category;
+                string Id = Guid.NewGuid().ToString();
+                entity.Id = Id;
+                entity.RowKey = Id;
+                var createdEntity = await _tablestorageService.GrocerryEntityDetails(entity);
+                return CreatedAtAction(nameof(InsertGroceryEntity), createdEntity);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Error try to add Grocery in Azure Table storage :{ex.StackTrace}{ex.InnerException}");
+                return StatusCode(StatusCodes.Status500InternalServerError,
+           "Error creating new grocery record in Azure Table");
+            }
         }
         [HttpPut]
-        public async Task<IActionResult> PutAsync([FromBody] GroceryItemEntity entity)
+        public async Task<IActionResult> UpdateGroceryEntity([FromBody] GroceryItemEntity entity)
         {
-            entity.PartitionKey = entity.Category;
-            entity.RowKey = entity.Id;
-            await _storageService.UpsertEntityAsync(entity);
-            return NoContent();
+            try
+            {
+                entity.PartitionKey = entity.Category;
+                entity.RowKey = entity.Id;
+                await _tablestorageService.GrocerryEntityDetails(entity);
+                return Ok("Updated GroceryEntity Details in Azure Table Successfully.");
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Error try to update Update GroceryEntity in Azure Table :{ex.StackTrace}{ex.InnerException}");
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                "Error updating Update GroceryEntity data in Azure Table");
+            }
         }
         [HttpDelete]
-        public async Task<IActionResult> DeleteAsync([FromQuery] string category, string id)
+        public async Task<IActionResult> DeleteGroceryEntity([FromQuery] string category, string id)
         {
-            await _storageService.DeleteEntityAsync(category, id);
-            return NoContent();
+            try
+            {
+                await _tablestorageService.DeleteGroceryDetails(category, id);
+                return Ok($"Deleted Grocery Details in Azure Table Based on id:{id},Category:{category}");
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError($"Error try to Delete GroceryEntity Details by category, id  in Azure Table:{id}{ex.StackTrace}{ex.InnerException}");
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                "Error Delete GroceryEntity data in Azure table");
+            }
         }
 
     }
