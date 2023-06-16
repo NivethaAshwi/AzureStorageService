90cf0130a1a795b500372bbfa1be3a9a49c3c3e3
NivethaAshwi <133736462+NivethaAshwi@users.noreply.github.com>
2023-06-16 10:29:12 +0530

    Update TableStorageService.cs

--- AzureStorage.Service/Service/TableStorageService.cs
+++ AzureStorage.Service/Service/TableStorageService.cs
@@ -23,27 +23,27 @@ namespace AzureStorage.Service.Service
         {
             _configuration = configuration;
         }
-        private async Task<TableClient> GetTableClient()
+       private async Task<TableClient> GetTableClient()
         {
             var serviceClient = new TableServiceClient(_configuration["StorageConnectionString"]);
             var tableClient = serviceClient.GetTableClient(TableName);
             await tableClient.CreateIfNotExistsAsync();
             return tableClient;
         }
-        public async  Task<GroceryItemEntity> GetEntityAsync(string category, string id)
+        public async  Task<GroceryItemEntity> GetGroceryDetails(string category, string id)
         {
             var tableClient = await GetTableClient();
             return await tableClient.GetEntityAsync<GroceryItemEntity>(category, id);
         }
 
-        public async Task<GroceryItemEntity> UpsertEntityAsync(GroceryItemEntity entity)
+        public async Task<GroceryItemEntity> GrocerryEntityDetails(GroceryItemEntity entity)
         {
             var tableClient = await GetTableClient();
             await tableClient.UpsertEntityAsync(entity);
             return entity;
         }
 
-        public async Task DeleteEntityAsync(string category, string id)
+        public async Task DeleteGroceryDetails(string category, string id)
         {
             var tableClient = await GetTableClient();
             await tableClient.DeleteEntityAsync(category, id);
